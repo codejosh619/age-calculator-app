5b9d1d2ca921c59a0a9ba6c98d56e1696ec320c4
codejosh619 <akumjoshuawin@gmail.com>
2025-02-19 23:40:39 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function calculateAge() {
+    const birthdate = document.getElementById('birthdate').value;
+    const birthDate = new Date(birthdate);
+    const today = new Date();
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+
+    // Adjust age if birthdate hasn't occurred yet this year
+    if (
+        monthDifference < 0 ||
+        (monthDifference === 0 && today.getDate() < birthDate.getDate())
+    ) {
+        age--;
+    }
+
+    // Display the results
+    document.getElementById('result').innerText = `Your age is ${age} years.`;
+}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered:', registration);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed:', error);
+      });
+  }
+  
